a860e3a1f2ce68faa61c34d705e09f6601c66cb2
Jacques <jacques.mironneau@gmail.com>
2020-11-06 14:11:25 +0100

    rectangle board initial test case

---
+++ src/fr/utt/lo02/projet/junit/RectangleBoardTest.java
@@ -0,0 +1,16 @@
+package fr.utt.lo02.projet.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RectangleBoardTest
+{
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
