358f16b6f066426f430d538a4b67c0d54ec4ce70
bapti <bapti@192.168.0.24>
2020-11-06 12:10:55 +0100

    Ajout Card

--- src/fr/utt/lo02/projet/Card.java
+++ src/fr/utt/lo02/projet/Card.java
@@ -1,5 +1,21 @@
 package fr.utt.lo02.projet;
 
 public class Card {
-
+	
+	public enum Color {BLUE, GREEN, RED};
+	
+	public enum Shape {CIRCLE, TRIANGLE, SQUARE};
+	
+	public enum Hollow {HOLLOW, FILLED};
+	
+	Color color;
+	Shape shape;
+	Hollow hollow;
+	
+	public Card (Color c, Shape s, Hollow h) {
+		this.color = c;
+		this.shape = s;
+		this.hollow = h;
+		
+	}
 }
