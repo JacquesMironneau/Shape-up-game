c6d675a348dc06ecfc4169e7f026786216240496
bapti <bapti@192.168.0.24>
2020-11-21 19:44:27 +0100

    Implementation Advanced mode

--- src/fr/utt/lo02/projet/game/ShapeUpGameAdvanced.java
+++ src/fr/utt/lo02/projet/game/ShapeUpGameAdvanced.java
@@ -1,10 +1,14 @@
 package fr.utt.lo02.projet.game;
 
-import fr.utt.lo02.projet.board.AbstractBoard;
-import fr.utt.lo02.projet.board.visitor.IBoardVisitor;
-import fr.utt.lo02.projet.strategy.PlayerStrategy;
-
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+
+import fr.utt.lo02.projet.board.AbstractBoard;
+import fr.utt.lo02.projet.board.Card;
+import fr.utt.lo02.projet.board.boardEmptyException;
+import fr.utt.lo02.projet.board.visitor.IBoardVisitor;
+import fr.utt.lo02.projet.strategy.*;
 
 public class ShapeUpGameAdvanced extends AbstractShapeUpGame
 {
@@ -14,39 +18,151 @@ public class ShapeUpGameAdvanced extends AbstractShapeUpGame
 		super(visitor, players, board);
 	}
 
+	/**
+	 * Initiate a round
+	 */
 	@Override
-	public void initRound()
+	protected void initRound()
 	{
-		// TODO Auto-generated method stub
+//		for (PlayerStrategy ps : players)
+//		{
+//			List<Card> list = ps.getPlayerHand();
+//			list = new ArrayList<>();
+//		}
+		for (PlayerStrategy player : players)
+		{
+			player.getPlayerHand().clear();
+		}
+		this.board.getPlacedCards().clear();
+		this.deck = new LinkedList<>();
+		initDeck();
 
+		Collections.shuffle((LinkedList<Card>) this.deck);
+
+		// Remove hidden card
+		this.deck.poll();
+		// Draw victory cards to players
+		for (PlayerStrategy player : players)
+		{
+			player.getPlayerHand().add(this.deck.poll());
+			player.getPlayerHand().add(this.deck.poll());
+			player.getPlayerHand().add(this.deck.poll());
+		}
+
+		this.isFirstTurn = true;
 	}
 
 	@Override
-	protected void playRound()
+	protected void playRound() throws PlayerHandEmptyException, boardEmptyException
 	{
-		// TODO Auto-generated method stub
+		PlayerStrategy player = players.get(0);
 
+		while (!isRoundFinished())
+		{
+			playTurn(player);
+			player = nextPlayer(player);
+		}
 	}
 
 	@Override
-	public void playTurn(PlayerStrategy player)
+	protected void playTurn(PlayerStrategy player) throws PlayerHandEmptyException, boardEmptyException
 	{
+
+		System.out.println(player.getName() +"'s turn !");
+		if (this.isFirstTurn)
+		{
+			PlaceRequest placeRequest;
+			do
+			{
+				placeRequest = player.askPlaceCard();
+			} while (!placeCardRequest(placeRequest, player));
+
+			// The first turn is finished
+			this.isFirstTurn = false;
+			board.display();
+			drawCard(player);		
+		} else
+		{
+			Choice choice;
+			do
+			{
+				choice = player.askChoice(ChoiceOrder.FIRST_CHOICE);
+			}
+			while (choice == Choice.END_THE_TURN);
+
+			switch (choice)
+			{
+				case PLACE_A_CARD -> {
+					PlaceRequest placeRequest;
+					do
+					{
+						placeRequest = player.askPlaceCard();
+					} while (!placeCardRequest(placeRequest, player));
+
+					board.display();
+
+					if (isRoundFinished()) return;
+					Choice secondChoice = player.askChoice(ChoiceOrder.SECOND_CHOICE);
+
+					if (secondChoice == Choice.MOVE_A_CARD)
+					{
+						MoveRequest request;
+						do
+						{
+							request = player.askMoveCard();
+						} while (!moveCardRequest(request));
+						board.display();
+
+					}
+				}
+				case MOVE_A_CARD -> {
+					MoveRequest request;
+					do
+					{
+						request = player.askMoveCard();
+					} while (!moveCardRequest(request));
+
+					board.display();
+
+					PlaceRequest placeRequest;
+					do
+					{
+						placeRequest = player.askPlaceCard();
+					} while (!placeCardRequest(placeRequest, player));
+					board.display();
+
+				}
+				default -> System.out.println("Error: end choice have been selected !");
+			}
+			if (!deck.isEmpty()) {
+			drawCard(player);
+			}
+		}
 
 	}
 
+
+	private PlayerStrategy nextPlayer(PlayerStrategy player)
+	{
+		return players.get((players.indexOf(player) + 1) % this.players.size());
+	}
 
 	@Override
-	protected void calculateGameScore()
+	protected  boolean isRoundFinished()
 	{
-		// TODO Auto-generated method stub
-
+		boolean victoryCards = false;
+		if(players.get(players.size()-1).getPlayerHand().size()-1==1) {
+			for (PlayerStrategy player : players)
+			{
+				player.setVictoryCard(player.getPlayerHand().get(0));
+			}
+			victoryCards = true;
+		}
+		for (PlayerStrategy player : players)
+		{
+			if (!(player.getPlayerHand().size()-1==1)) return false;
+		}
+		
+		return deck.isEmpty()&victoryCards;
 	}
-
-	@Override
-	protected boolean isRoundFinished()
-	{
-		return false;
-	}
-
-
 }
